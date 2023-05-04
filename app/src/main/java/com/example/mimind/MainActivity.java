96b0038a1babc4bdea7c0d240b71fafa26c714c6
user06 <45358>
2023-05-04 10:25:19 +0300

    init commit

--- app/src/main/java/com/example/mimind/MainActivity.java
+++ app/src/main/java/com/example/mimind/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.mimind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.window.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGHT = 5000;
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
